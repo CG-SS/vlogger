df19c24547c09801060dae2e7ae43b5336d6cb64
CG-SS <criguiss@uol.com.br>
2025-04-06 22:05:36 -0300

    Update example

--- main.v
+++ main.v
@@ -2,9 +2,17 @@ module main
 
 import vlogger
 import time
+import os
 
 fn main() {
-	logger := vlogger.DefaultLogger.new()
-	logger.info().bool('hello', false).send().send().send()
+	mut out := os.stdout()
+	defer { out.close() }
+
+	logger := vlogger.default(mut out)
+
+	logger.info().bool('hello', false).send()
+	logger.debug().u8('number', 8).string('test', 'this is a test').f32('float_val', 3.66).send()
+	logger.info().bool('hello', false).message('hello there')
+	logger.error().f64('number', 776.54323).message('another number')
 	time.sleep(1 * time.second)
 }
---
+++ nop_message_writer.v
@@ -0,0 +1,3 @@
+module vlogger
+
+fn nop_message_writer(_ Message) {}
