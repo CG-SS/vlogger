aea467187724aeb42b34e02f0c545459f4c7d0ce
CG-SS <criguiss@uol.com.br>
2025-04-08 21:49:49 -0300

    Change message append fns

--- src/logger.v
+++ src/logger.v
@@ -3,7 +3,7 @@ module vlogger
 import io
 
 // Logger can be seen as a Message factory. The idea is that a Message would carry all the necessary
-interface Logger {
+pub interface Logger {
 	trace() Message
 	debug() Message
 	info() Message
@@ -19,18 +19,28 @@ pub fn default(mut out io.Writer) Logger {
 }
 
 pub fn new(cfg LoggerCfg) Logger {
-	if cfg.level == MessageLevel.none {
+	message_cfg := cfg.message
+	if message_cfg.level == MessageLevel.none {
 		return NopLogger{}
 	}
 
 	mut hook_fns_new := cfg.hook_fns.clone()
-	hook_fns_new << append_timestamp_fn(AppendTimestampCfg{})
-	hook_fns_new << append_message_level_fn(default_level_fieldname)
+	hook_fns_new << append_message_level_fn(message_cfg.fieldname)
+
+	timestamp_cfg := cfg.timestamp
+
+	if timestamp_cfg.enabled {
+		if timestamp_cfg.format == .custom {
+			hook_fns_new << append_custom_timestamp_fn(timestamp_cfg.custom, timestamp_cfg.fieldname)
+		} else {
+			hook_fns_new << append_timestamp_fn(timestamp_cfg.format, timestamp_cfg.fieldname)
+		}
+	}
 
 	logger := DefaultLogger{
-		message_fieldname: cfg.message_fieldname
+		message_fieldname: message_cfg.fieldname
 		message_chan:      chan Message{cap: cfg.buffer_size}
-		level:             cfg.level
+		level:             message_cfg.level
 		write_fn:          cfg.write_fn
 		hook_fns:          hook_fns_new
 	}
