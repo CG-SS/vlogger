11ef41786d1d8d6aa31f26d73e2b44d6010085f0
CG-SS <criguiss@uol.com.br>
2025-04-08 21:48:54 -0300

    Change timestamp fn and new fn

--- src/timestamp.v
+++ src/timestamp.v
@@ -2,7 +2,7 @@ module vlogger
 
 import time
 
-enum TimestampType {
+enum TimestampFormat {
 	default
 	rfc3339
 	rfc3339_micro
@@ -18,55 +18,57 @@ enum TimestampType {
 	custom
 }
 
-struct AppendTimestampCfg {
-	timestamp_field_name string        = default_timestamp_fieldname
-	timestamp_type       TimestampType = .default
-	custom_format        string
-}
-
-fn append_timestamp_fn(cfg AppendTimestampCfg) HookFn {
-	return fn [cfg] (m Message) Message {
+fn append_custom_timestamp_fn(custom_format string, timestamp_field_name string) HookFn {
+	return fn [timestamp_field_name, custom_format] (msg Message) Message {
 		now := time.now()
 
-		match cfg.timestamp_type {
+		return msg.string(timestamp_field_name, now.custom_format(custom_format))
+	}
+}
+
+fn append_timestamp_fn(format TimestampFormat, timestamp_field_name string) HookFn {
+	return fn [format, timestamp_field_name] (msg Message) Message {
+		now := time.now()
+
+		match format {
 			.default {
-				return m.string(cfg.timestamp_field_name, now.format())
+				return msg.string(timestamp_field_name, now.format())
 			}
 			.rfc3339 {
-				return m.string(cfg.timestamp_field_name, now.format_rfc3339())
+				return msg.string(timestamp_field_name, now.format_rfc3339())
 			}
 			.rfc3339_micro {
-				return m.string(cfg.timestamp_field_name, now.format_rfc3339_micro())
+				return msg.string(timestamp_field_name, now.format_rfc3339_micro())
 			}
 			.rfc3339_nano {
-				return m.string(cfg.timestamp_field_name, now.format_rfc3339_nano())
+				return msg.string(timestamp_field_name, now.format_rfc3339_nano())
 			}
 			.ss {
-				return m.string(cfg.timestamp_field_name, now.format_ss())
+				return msg.string(timestamp_field_name, now.format_ss())
 			}
 			.ss_micro {
-				return m.string(cfg.timestamp_field_name, now.format_ss_micro())
+				return msg.string(timestamp_field_name, now.format_ss_micro())
 			}
 			.ss_milli {
-				return m.string(cfg.timestamp_field_name, now.format_ss_milli())
+				return msg.string(timestamp_field_name, now.format_ss_milli())
 			}
 			.ss_nano {
-				return m.string(cfg.timestamp_field_name, now.format_ss_nano())
+				return msg.string(timestamp_field_name, now.format_ss_nano())
 			}
 			.unix {
-				return m.i64(cfg.timestamp_field_name, now.unix())
+				return msg.i64(timestamp_field_name, now.unix())
 			}
 			.unix_micro {
-				return m.i64(cfg.timestamp_field_name, now.unix_micro())
+				return msg.i64(timestamp_field_name, now.unix_micro())
 			}
 			.unix_milli {
-				return m.i64(cfg.timestamp_field_name, now.unix_milli())
+				return msg.i64(timestamp_field_name, now.unix_milli())
 			}
 			.unix_nano {
-				return m.i64(cfg.timestamp_field_name, now.unix_nano())
+				return msg.i64(timestamp_field_name, now.unix_nano())
 			}
-			.custom {
-				return m.string(cfg.timestamp_field_name, now.custom_format(cfg.custom_format))
+			else {
+				return msg
 			}
 		}
 	}
