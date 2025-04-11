3c588c94979c4aebe83bcdacbd554e3628332753
CG-SS <criguiss@uol.com.br>
2025-04-11 18:12:15 -0300

    Update example

--- examples/default/main.v
+++ examples/default/main.v
@@ -4,15 +4,39 @@ import vlogger
 import time
 import os
 
+struct LoggableStrut {
+	name string
+	age  i32
+}
+
+fn (l LoggableStrut) fields() []vlogger.Field {
+	return [
+		vlogger.Field{
+			key:   'name'
+			value: vlogger.StringValue{l.name}
+		},
+		vlogger.Field{
+			key:   'age'
+			value: vlogger.I32Value{l.age}
+		},
+	]
+}
+
 fn main() {
 	mut out := os.stdout()
 	defer { out.close() }
 
 	logger := vlogger.default(mut out)
 
-	logger.info().bool('hello', false).send()
+	logger.trace().bool('hello', false).send()
 	logger.debug().u8('number', 8).string('test', 'this is a test').f32('float_val', 3.66).send()
 	logger.info().bool('hello', false).message('hello there')
-	logger.error().f64('number', 776.54323).message('another number')
+	logger.warn().f64('number', 776.54323).message('another number')
+
+	logger.error().strut('struct', LoggableStrut{
+		name: 'John'
+		age:  19
+	}).send()
+	logger.fatal().err('error', error('failure')).send()
 	time.sleep(1 * time.second)
 }
